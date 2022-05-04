859d85eaf29d58b60b518717bccafd47394ce767
mark <mddoyne@asu.edu>
2022-05-04 16:03:01 -0600

    save

---
+++ index.js
@@ -0,0 +1,3 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+const square = (parameter1, parameter2) => Math.pow(parameter1, 2);
+const add = (parameter1, parameter2) => parameter1 + parameter2;
